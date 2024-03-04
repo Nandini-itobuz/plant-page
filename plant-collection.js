2ce6b4e952644f8cb443154cc9deba7603615bc1
itobuz <itobuz@itobuzs-MacBook-Air.local>
2024-03-04 18:40:07 +0530

    first commit

---
+++ plant-collection.js
@@ -0,0 +1,6 @@
+const plantCollectionArray = ['images/plant-collection/paradise.png','images/plant-collection/pearls.png','images/plant-collection/rubber.png'];
+const nextbtn = document.getElementsByClassName('next-plant-collection');
+
+plantCollectionArray.forEach(ele =>{
+    
+})
